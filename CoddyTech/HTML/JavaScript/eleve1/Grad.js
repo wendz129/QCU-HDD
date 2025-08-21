07f9e7127d0d13fdd7150c71861b9e54b332919a
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-08-21 20:54:18 +0800

    Add files via upload

---
+++ CoddyTech/HTML/JavaScript/eleve1/Grad.js
@@ -0,0 +1,10 @@
+const food = "Franklin Benjamin"
+const gad = "no dropped off location"
+
+// FB
+
+const fb = "This is not book it's"
+
+console.log(`This is graduation = ${food}`)
+console.log(`Hard to get = ${gad}`)
+console.log(`Blog = ${fb}`)
